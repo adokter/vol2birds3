d7db109ea2e0c4ab9d19799dad0946ea4320c6a8
Adriaan Dokter <adriaandokter@gmail.com>
2017-02-01 22:14:19 -0500

    fixes and simplifications

--- bin/process_file.rb
+++ bin/process_file.rb
@@ -8,22 +8,12 @@ Aws.config.update({
 
 data_file = ENV['DATAFILE']
 output_bucket = ENV['DEST_BUCKET']
-output_file = File.basename(data_file)
+output_file = File.basename("#{data_file}")
 s3 = Aws::S3::Client.new
 
-if data_file.include?(".gz")
+resp = s3.get_object(response_target: "#{output_file}",bucket: 'noaa-nexrad-level2',key: data_file)
 
-  gz = Zlib::GzipReader.new(s3.get_object(bucket: 'noaa-nexrad-level2', key: data_file).body)
-  File.open('data', 'w') { |file| file.write(gz.read) }
-  gz.close
-else
-  resp = s3.get_object(
-   response_target: 'data',
-   bucket: 'noaa-nexrad-level2',
-   key: data_file)
-end
-
-`vol2bird data output`
+`vol2bird #{output_file} output`
 
 # upload file from disk in a single request, may not exceed 5GB
 File.open('output', 'rb') do |file|
