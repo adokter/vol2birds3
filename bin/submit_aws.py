3b63d5acc43b7ef9781d29825c2e06420df34342
Adriaan Dokter <adriaandokter@gmail.com>
2017-04-18 18:09:43 -0400

    loop for submit_aws

--- bin/submit_aws.py
+++ bin/submit_aws.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python
 import boto3
+import pytz
+from datetime import timedelta
+from datetime import datetime
+
+utc=pytz.UTC
 
 client = boto3.client('batch')
 
-params={'radar':'KBRO', 'date':'2015/05/10'}
+mydate='2015/03/01'
+myradar='KBRO'
+mydays=122
 
-response = client.submit_job(
-    jobDefinition='vol2birds3-job:12',
-    jobName='bototest',
-    jobQueue='spot20-job-queue',
-    parameters=params
-)
+mydatetime = utc.localize(datetime.strptime(mydate, '%Y/%m/%d'))
+datetimes = [mydatetime+timedelta(days=x) for x in range(0,mydays)]
 
-print(response)
+for t in datetimes:
+   params={'radar':myradar, 'date':t.strftime("%Y/%m/%d")}
+   response = client.submit_job(
+   jobDefinition='vol2birds3-job:12',
+   jobName='bototest',
+   jobQueue='spot20-job-queue',
+   parameters=params)
+   print params
+
