41d89dbffad9e6e3b25c4a1fedc0b58ffc9b526b
Adriaan Dokter <adriaandokter@gmail.com>
2017-10-16 17:05:16 -0400

    added output folder argument

--- scar/make_lambda_function.sh
+++ scar/make_lambda_function.sh
@@ -4,4 +4,4 @@ export scar=~/git/scar/scar.py
 # remove any existing instances of the lambda function
 $scar rm -n lambda-docker-vol2bird
 # make lambda function named lambda-docker-vol2bird, using 128 Mb memory, from Docker image adokter/lambda_vol2bird
-$scar init -s vol2bird_scar.sh -n lambda-docker-vol2bird -m 128 -es vol2bird adokter/lambda_vol2bird
+$scar init -s vol2bird_scar.sh -n lambda-docker-vol2bird -m 128 -es vol2bird -o vol2bird adokter/lambda_vol2bird
