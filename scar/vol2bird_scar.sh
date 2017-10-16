ab6652749e4f37a49b3af6c15088b28a857955b6
Adriaan Dokter <adriaandokter@gmail.com>
2017-10-16 18:02:44 -0400

    added date, time and radar selection

--- scar/vol2bird_scar.sh
+++ scar/vol2bird_scar.sh
@@ -9,6 +9,34 @@
 OUTPUT_DIR="/tmp/$REQUEST_ID/output"
 
 echo "SCRIPT: Invoked vol2bird. File available in $SCAR_INPUT_FILE"
+
+SCAR_INPUT_FILE="KVNX20171405_154507_V06"
+
 FILE_NAME=`basename $SCAR_INPUT_FILE`
 OUTPUT_FILE=$OUTPUT_DIR/$FILE_NAME".h5"
+
+# do some selection on date
+DATE_FILE=${FILE_NAME:4:12}
+HOUR_FILE=${FILE_NAME:13:2}
+DATE_MIN=`date --date="7 days ago" +"%Y%m%d"`
+RADAR=${FILE_NAME:0:4}
+
+if [ ${RADAR:0:1} != "K" ];
+then 
+    echo "SCRIPT: not a USA radar, ignoring ...";
+    exit 1
+fi;
+
+if [ $DATE_FILE \< $DATE_MIN ];
+then 
+    echo "SCRIPT: file over a week old, ignoring...";
+    exit 1
+fi;
+
+if [ $HOUR_FILE \< 22 ] && [ $HOUR_FILE \> 14 ];
+then
+    echo "SCRIPT: daytime, ignoring ...";
+    exit 1
+fi;
+
 vol2bird $SCAR_INPUT_FILE $OUTPUT_FILE > /dev/null
