b77c12f09f50a2e373366b78abafa903d1540946
Adriaan Dokter <adriaandokter@gmail.com>
2017-10-23 17:22:12 -0400

    added nexrad options

--- scar/vol2bird_scar.sh
+++ scar/vol2bird_scar.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # 1. A file uploaded to the input folder of the S3 bucket will be made available 
 #    for the container in /tmp/$REQUEST_ID/input. The path to the file is in $SCAR_INPUT_FILE
@@ -8,33 +8,34 @@
 
 OUTPUT_DIR="/tmp/$REQUEST_ID/output"
 
-echo "SCRIPT: Invoked vol2bird. File available in $SCAR_INPUT_FILE"
+echo "SCRIPT: Invoked processing script. File available in $SCAR_INPUT_FILE"
 
 FILE_NAME=`basename $SCAR_INPUT_FILE`
 OUTPUT_FILE=$OUTPUT_DIR/$FILE_NAME".h5"
 
 # do some selection on date
-DATE_FILE=${FILE_NAME:4:12}
-HOUR_FILE=${FILE_NAME:13:2}
-DATE_MIN=`date --date="7 days ago" +"%Y%m%d"`
-RADAR=${FILE_NAME:0:4}
+#DATE_FILE=${FILE_NAME:4:8}
+#HOUR_FILE=${FILE_NAME:13:2}
+#DATE_MIN=`date --date="7 days ago" +"%Y%m%d"`
+#RADAR=${FILE_NAME:0:4}
 
-if [ ${RADAR:0:1} != "K" ];
-then 
-    echo "SCRIPT: not a USA radar, ignoring ...";
-    exit 1
-fi;
+#if [ ${RADAR:0:1} != "K" ];
+#then 
+#    echo "SCRIPT: not a USA radar, ignoring ...";
+#    exit 1
+#fi;
 
-if [ $DATE_FILE \< $DATE_MIN ];
-then 
-    echo "SCRIPT: file over a week old, ignoring...";
-    exit 1
-fi;
+#if [ $DATE_FILE \< $DATE_MIN ];
+#then 
+#    echo "SCRIPT: file over a week old, ignoring...";
+#    exit 1
+#fi;
 
-if [ $HOUR_FILE \< 22 ] && [ $HOUR_FILE \> 14 ];
-then
-    echo "SCRIPT: daytime, ignoring ...";
-    exit 1
-fi;
+#if [ $HOUR_FILE \< 22 ] && [ $HOUR_FILE \> 14 ];
+#then
+#    echo "SCRIPT: daytime, ignoring ...";
+#    exit 1
+#fi;
 
+echo "SCRIPT: invoked vol2bird";
 vol2bird $SCAR_INPUT_FILE $OUTPUT_FILE > /dev/null
